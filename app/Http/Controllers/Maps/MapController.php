4bbac60d836b804cc41211822b9a03b517e3d022
LittleElijah <cuiyeqing960904@gmail.com>
2018-08-02 16:41:56 +0300

    integrated google-map with laravel

---
+++ app/Http/Controllers/Maps/MapController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Maps;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class MapController extends Controller
+{
+    public function index()
+    {
+        
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,19 @@
 <?php
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/auth/google', 'Auth\LoginController@redirectToProvider')->name('google.auth');
-Route::get('/auth/google/callback', 'Auth\LoginController@handleProviderCallback')->name('google.callback');
+/**
+ * Authentication
+ */
+Route::group(['prefix' => 'auth'], function () {
+    Route::get('/google', 'Auth\LoginController@redirectToProvider')->name('google.auth');
+    Route::get('/google/callback', 'Auth\LoginController@handleProviderCallback')->name('google.callback');
+});
+
+/**
+ * Map
+ */
+Route::group(['prefix' => 'maps'], function () {
+
+});
